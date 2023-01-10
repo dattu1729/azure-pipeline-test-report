a2f409ceb61c54d63831ecaae4fe13700d1194d8
dattu1729 <dattu.1729@gmail.com>
2023-01-10 08:58:58 +0530

    service connection

--- tasks/generate-test-report/index.ts
+++ tasks/generate-test-report/index.ts
@@ -1,9 +1,27 @@
-import * as tl from "azure-pipelines-task-lib/task";
+import * as tl from 'azure-pipelines-task-lib/task';
 
 async function run() {
   try {
-    const reqIds: string | undefined = tl.getInput('reqIds', true);
+    const reqIds: string = tl.getInputRequired('reqIds');
     console.log('Configured requirement Ids: ', reqIds);
+    const serviceConnectionName = tl.getInput('MyService', true);
+    console.log('serviceConnectionName', serviceConnectionName);
+    if (serviceConnectionName) {
+      const url = tl.getEndpointUrl(serviceConnectionName, false);
+      const token = tl.getEndpointAuthorizationParameter(
+        serviceConnectionName,
+        'apitoken',
+        false
+      );
+      const proxyUrl = tl.getEndpointDataParameter(
+        serviceConnectionName,
+        'proxy',
+        true
+      );
+      console.log('service-url', url);
+      console.log('service-token', token);
+      console.log('service-proxyUrl', proxyUrl);
+    }
   } catch (err: any) {
     console.error(err);
     tl.setResult(tl.TaskResult.Failed, err.message);
